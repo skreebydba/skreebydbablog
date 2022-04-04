b0de24ecdcb4f1b9fe3b4bcf58b258bb3eba1f20
Frank Gill <fgill@fortifieddata.com>
2022-04-04 10:04:24 -0500

    Initial commit of Install-AzureInit

---
+++ AzureInit/Install-AzureInit.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression; 
+choco install zoomit -y; 
+choco install notepadplusplus -y; 
+choco install git -y; 
+choco install azure-data-studio -y; 
+choco install microsoft-edge -y; 
+choco install powershell-core -y; 
